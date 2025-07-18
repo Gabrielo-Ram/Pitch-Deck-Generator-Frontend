cec6057ff0e616a62de68c8278dba7a66beb222b
26gdr2 <gdr2@williams.edu>
2025-07-18 14:52:38 -0400

    Set variables to production

--- clientFrontend/src/components/Home.jsx
+++ clientFrontend/src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
 
   let BACKEND_URL = "";
   //Change to false if in production
-  true
+  false
     ? (BACKEND_URL = "http://localhost:3001")
     : (BACKEND_URL = "https://csv-to-slides-web-app.onrender.com");
 
